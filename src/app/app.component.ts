2fda290c7a5a7c9f89d97939250236e0ec2d81f8
Alexander Tsuman <otsuman@productengine.com>
2018-04-11 12:33:24 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
+import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //templateUrl: './app.component.html',
+  template: `
+  <p [@myAwesomeAnimation]='state' (click)='animateMe()'>{{ someProperty }}</p>
+  `,
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('myAwesomeAnimation', [
+      
+      state('small', style({
+        transform: 'scale(1)'
+      })),
+      state('large', style({
+        transform: 'scale(1.2)'
+      })),
+
+      transition('small <=> large', animate('300ms ease-in', keyframes([
+        style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+        style({opacity: 1, transform: 'translateY(35px)', offset: .5}),
+        style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+      ]))),
+
+    ]),
+
+  ]
 })
 export class AppComponent {
-  title = 'app';
+
+  constructor(private dataService:DataService) {
+
+  }
+  
+  state:string = 'small';
+
+  animateMe() {
+    this.state = this.state === 'small' ? 'large' : 'small';
+  }
+
+  someProperty:string = '';
+
+  ngOnInit() {
+    console.log(this.dataService.cars);
+
+    this.someProperty = this.dataService.myData();
+  }
+
+  title = 'My First Angular App';
 }
